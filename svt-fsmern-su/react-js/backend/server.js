bd61802cbcf5634ece3b22509c1276bf3df7a85c
Subho44 <98902921+Subho44@users.noreply.github.com>
2025-07-24 13:39:47 +0530

    auth backend
    done

--- svt-fsmern-su/react-js/backend/server.js
+++ svt-fsmern-su/react-js/backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectdb = require("./config/db");
 const clientRoutes = require("./routes/clientRoutes");
-
+const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 connectdb();
 const app = express();
@@ -11,6 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/clients",clientRoutes);
-
+app.use("/api/auth",authRoutes);
 const PORT = process.env.PORT || 5700;
 app.listen(PORT,()=>console.log('server is running port 5700'));
